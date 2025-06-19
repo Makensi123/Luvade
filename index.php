bc2acc31240dd37968dccbdf65e8bf3c61eab98c
Makensi123 <fabriciomakensi@gmail.com>
2025-06-18 23:32:04 -0500

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("config/bd.php");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Gestion Empresa</title>
+	<link rel="stylesheet" href="extjs/resources/css/ext-all.css">
+	<script src="extjs/ext-all.js"></script>
+	<script src="app.js"></script>
+</head>
+
+<body></body>
+
+</html>
